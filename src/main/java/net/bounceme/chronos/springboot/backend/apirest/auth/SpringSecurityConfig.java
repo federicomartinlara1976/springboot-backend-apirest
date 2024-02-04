60da48decda5bfc84f5bd0f565eb1d61809fdfbe
Federico Martín Lara <federico@horizontefinal>
2024-02-04 10:31:54 +0100

    supress warning

--- src/main/java/net/bounceme/chronos/springboot/backend/apirest/auth/SpringSecurityConfig.java
+++ src/main/java/net/bounceme/chronos/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
