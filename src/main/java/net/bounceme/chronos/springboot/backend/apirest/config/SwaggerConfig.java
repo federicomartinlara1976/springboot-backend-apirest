1cace50994f0b362527b55739f1578f2ad53e72b
Federico Martín Lara <federico@horizontefinal>
2024-02-04 11:42:14 +0100

    Swagger

---
+++ src/main/java/net/bounceme/chronos/springboot/backend/apirest/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package net.bounceme.chronos.springboot.backend.apirest.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("net.bounceme.chronos.springboot.backend.apirest.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(getApiInfo())
+				;
+	}
+	
+	private ApiInfo getApiInfo() {
+		return new ApiInfo(
+				"SpringBoot backend apirest controller",
+				"Api de control para SpringBoot backend apirest",
+				"0.0.1-SNAPSHOT",
+				"https://github.com/federicomartinlara1976/springboot-backend-apirest/blob/master/LICENSE",
+				new Contact("CHGuadalquivir", "https://github.com/federicomartinlara1976/springboot-backend-apirest", "federicomartinlara1976@gmail.com"),
+				"LICENSE",
+				"LICENSE URL",
+				Collections.emptyList()
+				);
+	}
+}
